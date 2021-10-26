ff6e333e600dd59ab45e2a7e497c793d1d625d5b
[1937forza] <[kadirofff5@outlook.com]>
2021-10-26 12:45:18 +0400

    HomeWork

---
+++ HomeWork/HomeWork/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Daxil edilmis string deyerinin icinde reqem olub olmadigini tapan proqram..... -_-
+
+
+            //char[] digits = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', };
+
+            //Console.WriteLine("Bir cumle yazin");
+
+            //string sntc = Console.ReadLine();
+
+            //for(int i = 0; i < sntc.Length; i++)
+            //{
+
+            //    for(int j = 0; j < digits.Length; j++)
+            //    {
+            //        bool check = false;
+            //        if (sntc[i] == digits[j])
+            //        {
+            //            check = true;
+            //            break;
+            //        }
+            //        if (check)
+            //            Console.WriteLine("Metnde reqemlerden istifade olunub !");
+            //        else
+            //        {
+            //            Console.WriteLine("Metnde reqemlerden istifade olunmayib !");
+            //            break;
+            //        }
+                        
+
+            //    }
+                
+
+            //}
+            
+
+
+            //3. Verilmis dedin reqemlerinin ededi ortasini tapan proqram
+
+            //int N =26;
+            //int sum = 0;
+            //int counter = 0;
+            //int mid = 0;
+
+            //while (N > 0)
+            //{
+            //    sum += N % 10;
+            //    N = (N - N % 10) / 10;
+            //    counter++;
+
+            //}
+            //if (counter > 0)
+            //{
+            //    mid = sum / counter;
+            //}
+            //else
+            //{
+            //    Console.WriteLine(N);
+            //}
+            //Console.WriteLine(mid);
+
+        }
+    }    
+
+}
+
